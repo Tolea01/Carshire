c7319e39a5e30a870bbc45948bf0df4653d1c4bd
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-07-09 19:29:56 +0300

    fixed menu

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,15 @@ const burget_menu = document.querySelector('.burger');
 burget_menu.addEventListener('click', () =>{
     burget_menu.classList.toggle('burger-active')
 })
+
+window.onscroll = () => {
+    let header = document.querySelector('.navbar');
+
+    if (window.pageYOffset > 50) {
+        header.classList.add('fixed-top');
+    }
+
+    else {
+        header.classList.remove('fixed-top')
+    }
+}
