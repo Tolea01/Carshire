78d21620e0d618ae5fe4cf6e11b0a26545816d9b
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-07-16 22:58:49 +0300

    price-filter

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ burget_menu.addEventListener('click', () =>{
     burget_menu.classList.toggle('burger-active')
 })
 
+
 window.onscroll = () => {
     let header = document.querySelector('.navbar');
 
@@ -13,4 +14,11 @@ window.onscroll = () => {
     else {
         header.classList.remove('fixed-top')
     }
+}
+
+
+let priceSlider = document.getElementById("price-slider");
+let selectValue = document.getElementById("select-value");
+priceSlider.oninput = function () {
+    selectValue.style.left = this.value + "%";
 }
